9df91539ffc44dd7bbec9947a6d05661f2b7e62a
mohanvishwakarma327 <santyvishwakarma694@gmail.com>
2025-01-21 23:40:20 +0530

    update on github

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import './login.jsx'
+import './Login.css'
+import './product.jsx'
+import Login from './login.jsx'
+function App() {
+
+
+  return (
+  <> 
+  <Login/>
+
+  </>
+    
+  )
+     
+}
+
+export default App
---
+++ src/product.jsx
@@ -0,0 +1,9 @@
+function product() {
+  return (
+    <div>
+      <h1>Product</h1>
+      <h2>Discription</h2>
+    </div>
+  );
+}
+export default product;
